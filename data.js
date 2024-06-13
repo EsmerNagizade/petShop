9b9ba1249b23ace75c008f658fae0a1c66617b04
Esmer Nagizade <esmer1234@gmail.com>
2024-06-13 19:32:41 +0400

    uptadet

---
+++ data.js
@@ -0,0 +1,39 @@
+let product=[
+
+{
+    id:1,
+    img:"./image/closeup-shot-ginger-cat-wicker-basket-isolated-white-wall_1-removebg-preview.png",
+    name:'Pişik evi',
+    price:42
+},
+
+{
+    id:2,
+    img:"./image/street-fashioned-brown-white-dog-cool-black-hoodie-trucker-cap-with-mesh-back-rustic-wooden-table 1.png",
+    name:'  İt üçün köynək',
+    price:60
+},
+{
+    id:3,
+    img:"./image/pets-food-wooden-floor 1.png",
+    name:'Yemək qabları',
+    price:35
+},
+{
+    id:4,
+    img:"./image/image 1 (1).png",
+    name:'Müxtəlif tasmalar',
+    price:42
+}
+
+
+
+
+
+
+
+]
+
+let basket=JSON.parse(localStorage.getItem('products'))||[
+
+]
