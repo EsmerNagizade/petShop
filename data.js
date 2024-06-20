57c43ed199a8ca8c71f3b04db9159a4b474ec3a9
Esmer Nagizade <esmer1234@gmail.com>
2024-06-20 14:18:08 +0400

    updated

--- data.js
+++ data.js
@@ -1,39 +1,91 @@
-let product=[
+let product = [
 
-{
-    id:1,
-    img:"./image/closeup-shot-ginger-cat-wicker-basket-isolated-white-wall_1-removebg-preview.png",
-    name:'Pişik evi',
-    price:42
-},
+    {
+        id: 1,
+        img: "./image/closeup-shot-ginger-cat-wicker-basket-isolated-white-wall_1-removebg-preview.png",
+        name: 'Pişik evi',
+        price: 42
+    },
 
-{
-    id:2,
-    img:"./image/street-fashioned-brown-white-dog-cool-black-hoodie-trucker-cap-with-mesh-back-rustic-wooden-table 1.png",
-    name:'  İt üçün köynək',
-    price:60
-},
-{
-    id:3,
-    img:"./image/pets-food-wooden-floor 1.png",
-    name:'Yemək qabları',
-    price:35
-},
-{
-    id:4,
-    img:"./image/image 1 (1).png",
-    name:'Müxtəlif tasmalar',
-    price:42
-}
+    {
+        id: 2,
+        img: "./image/street-fashioned-brown-white-dog-cool-black-hoodie-trucker-cap-with-mesh-back-rustic-wooden-table 1.png",
+        name: '  İt üçün köynək',
+        price: 60
+    },
+    {
+        id: 3,
+        img: "./image/pets-food-wooden-floor 1.png",
+        name: 'Yemək qabları',
+        price: 35
+    },
+    {
+        id: 4,
+        img: "./image/image 1 (1).png",
+        name: 'Müxtəlif tasmalar',
+        price: 42
+    }
+];
 
-
-
-
-
+let basket = JSON.parse(localStorage.getItem("products")) || [];
 
 
-]
 
-let basket=JSON.parse(localStorage.getItem('products'))||[
-
-]
+let product2 = [
+    {
+        id: 5,
+        img: './image/Me-O Cat Food - Pets - Package Inspiration 1.png',
+        name: "Pişiklər üçün quru və nəm lasos",
+        price: 45,
+        x:"secilenmehsullar"
+    },
+    {
+        id: 6,
+        img: './image/Natures Nuts 00029 8 Lbs Premium Safflower Seed (Pack of 4) 1.png',
+        name: "Quşlar üçün çərəz",
+        price: 9.90,
+         x:"yenimehsullar"
+    },
+    {
+        id: 7,
+        img: './image/image 5.png',
+        name: "Balıqlar üçün quru toyuq",
+        price:19,
+         x:"yenimehsullar"
+    },
+    {
+        id: 8,
+        img: './image/Dog Friendly Kennebunk, Maine - Daily Dog Tag 1.png',
+        name: "İtlər üçün sümüklər",
+        price: 33,
+         x:"encoxmehsullar"
+    },
+    {
+        id: 9,
+        img: './image/JINX_ Premium Dog Food Made for Modern Dogs 1.png',
+        name: "Pişiklər üçün çubuq",
+        price: 40,
+         x:"secilenmehsullar"
+    },
+    {
+        id: 10,
+        img: './image/Wagg Sets Tails Going With a Rebrand by Robot Food 1.png',
+        name: "Dovşanlar üçün dəri ət",
+        price: 13.9,
+         x:"yenimehsullar"
+    },
+    {
+        id: 11,
+        img: './image/Kasper Faunafood Caviakorrel - Caviavoer - 4 kg 1.png',
+        name: "Balıqlar üçün quru çörək",
+        price: 25,
+         x:"encoxmehsullar"
+    },
+    {
+        id: 12,
+        img: './image/image 4.png',
+        name: "İtlər üçün quru yemək və ət",
+        price:40 ,
+         x:"yenimehsullar"
+    }
+];
--- sebet.js
+++ sebet.js
@@ -1,17 +1,142 @@
-basket.forEach(data=>{
-      newTrendContainer.innerHTML+=`
-          <div class="new_trend_card">
-            <div class="new_trend_img">
-    <img src="${data.img}" alt="">
+
+
+let newTrendContainer=document.querySelector('.new_trend_container')
+
+
+// Render the basket items
+// function renderBasket() {
+//   newTrendContainer.innerHTML = '';
+//   basket.forEach(data => {
+//       newTrendContainer.innerHTML += `
+//           <div class="new_trend_card">
+//               <div class="new_trend_img">
+//                   <img src="${data.img}" alt="">
+//               </div>
+//               <p>
+//                   ${data.name}
+//                   <span>${data.price * data.count}</span>
+//                   <svg width="11" height="12" viewBox="0 0 11 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+//                       <path d="M10.7819 6.42857V11.1429C10.7819 11.6167 10.3751 12 9.87343 12C9.37179 12 8.96497 11.6167 8.96497 11.1429V6.42857C8.96497 5.07429 8.00257 3.94018 6.69382 3.56786V11.1429C6.69382 11.6167 6.287 12 5.78535 12C5.28371 12 4.87689 11.6167 4.87689 11.1429V3.56786C3.56814 3.94018 2.60574 5.07321 2.60574 6.42857V11.1429C2.60574 11.6167 2.19892 12 1.69728 12C1.19564 12 0.788818 11.6167 0.788818 11.1429V6.42857C0.788818 4.12259 2.55464 2.20179 4.87689 1.79732V0.857143C4.87689 0.383304 5.28371 0 5.78535 0C6.287 0 6.69382 0.383304 6.69382 0.857143V1.79625C9.01607 2.20179 10.7819 4.12232 10.7819 6.42857Z" fill="black"/>
+//                   </svg>
+//               </p>
+//               <div>
+//                   <button onclick="minus(${data.id})">-</button>
+//                   <p>${data.count}</p>
+//                   <button onclick="plus(${data.id})">+</button>
+//               </div>
+//           </div>
+//       `;
+//   });
+// }
+
+// // Function to increase the count of a product in the basket
+// function plus(id) {
+//   let checkbasket = basket.find(data => data.id === id);
+//   if (checkbasket) {
+//       checkbasket.count++;
+//   } else {
+//       console.error(`Product with id ${id} not found in basket`);
+//   }
+//   localStorage.setItem("products", JSON.stringify(basket));
+//   renderBasket();
+// }
+
+// // Function to decrease the count of a product in the basket
+// function minus(id) {
+//   let checkbasket = basket.find(data => data.id === id);
+//   if (checkbasket && checkbasket.count > 0) {
+//       checkbasket.count--;
+//       if (checkbasket.count === 0) {
+//           basket = basket.filter(data => data.id !== id);
+//       }
+//   } else {
+//       console.error(`Product with id ${id} not found in basket or count is already 0`);
+//   }
+//   localStorage.setItem("products", JSON.stringify(basket));
+//   renderBasket();
+// }
+
+// // Initial render of the basket
+// renderBasket();
+
+function basketislet() {
+   newTrendContainer.innerHTML='';
+  basket.forEach(data=>{
+   
+        newTrendContainer.innerHTML+=`
+            <div class="new_trend_card">
+              <div class="new_trend_img">
+      <img src="${data.img}" alt="">
+    
+              </div>
+              <p>
+                ${data.name}
+                  <span>${data.price*data.count}</span>
   
-            </div>
-            <p>
-              ${data.name}
-                <span>${data.price}</span>
-                <svg width="11" height="12" viewBox="0 0 11 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M10.7819 6.42857V11.1429C10.7819 11.6167 10.3751 12 9.87343 12C9.37179 12 8.96497 11.6167 8.96497 11.1429V6.42857C8.96497 5.07429 8.00257 3.94018 6.69382 3.56786V11.1429C6.69382 11.6167 6.287 12 5.78535 12C5.28371 12 4.87689 11.6167 4.87689 11.1429V3.56786C3.56814 3.94018 2.60574 5.07321 2.60574 6.42857V11.1429C2.60574 11.6167 2.19892 12 1.69728 12C1.19564 12 0.788818 11.6167 0.788818 11.1429V6.42857C0.788818 4.12259 2.55464 2.20179 4.87689 1.79732V0.857143C4.87689 0.383304 5.28371 0 5.78535 0C6.287 0 6.69382 0.383304 6.69382 0.857143V1.79625C9.01607 2.20179 10.7819 4.12232 10.7819 6.42857Z" fill="black"/>
-                </svg>
-            </p>
-        </div>
-      `
-    })
+                  <svg width="11" height="12" viewBox="0 0 11 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M10.7819 6.42857V11.1429C10.7819 11.6167 10.3751 12 9.87343 12C9.37179 12 8.96497 11.6167 8.96497 11.1429V6.42857C8.96497 5.07429 8.00257 3.94018 6.69382 3.56786V11.1429C6.69382 11.6167 6.287 12 5.78535 12C5.28371 12 4.87689 11.6167 4.87689 11.1429V3.56786C3.56814 3.94018 2.60574 5.07321 2.60574 6.42857V11.1429C2.60574 11.6167 2.19892 12 1.69728 12C1.19564 12 0.788818 11.6167 0.788818 11.1429V6.42857C0.788818 4.12259 2.55464 2.20179 4.87689 1.79732V0.857143C4.87689 0.383304 5.28371 0 5.78535 0C6.287 0 6.69382 0.383304 6.69382 0.857143V1.79625C9.01607 2.20179 10.7819 4.12232 10.7819 6.42857Z" fill="black"/>
+                  </svg>
+              </p>
+              <div>
+             <button onclick="minus(${data.id})">-</button>
+              
+              <p>${data.count}</p>
+                <button onclick="plus(${data.id})">+</button>
+              </div>
+          </div>
+        `
+      })
+}
+
+window.addEventListener("load",()=>{
+  basketislet()
+})
+    // function plus(z) {
+    //   let checkbasket=basket.find(data=>data.id===z)
+    //   if (checkbasket) {
+    //     checkbasket.count++
+    //     basket.push(checkbasket)
+    //   }
+    //   else{
+
+    //   }
+    //   localStorage.setItem("products",JSON.stringify(checkbasket))
+    //   basketislet()
+    // }
+
+    function plus(x) {
+      let check=basket.find(data=>data.id===x)
+      if (check){
+        check.count++
+      }else{
+
+      }
+      localStorage.setItem("products",JSON.stringify(check))
+       basketislet()
+    }
+
+    function minus(x) {
+      let check=basket.find(data=>data.id===x)
+      if (check && check.count>0){
+
+        check.count--
+        
+        
+        if (check.count===0) {
+          basket = basket.filter(data=>data.id!==x)
+        }
+      }
+      else{
+      // let index=basket.filter(data=>data.id!==x)
+      // if (index){
+      
+      //   basketislet()
+      }
+
+      localStorage.setItem("products",JSON.stringify(basket))
+      basketislet()
+      }
+    
+
+    let newTrendCard=document.querySelectorAll(".new_trend_card")
+    
